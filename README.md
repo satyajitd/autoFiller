7b338ad532c5dcdce5a8ef2413b33e81d020d367
Satyajit Das <satyajitd1998@gmail.com>
2020-04-21 23:47:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# autoFiller
+
+This is an attempt to fill a Google form automatically by extracting data from xlsx file.
+For this to work, the data in the xlsx file should be in correspondence to the Google form format.
+
+# Execution Instructions
+
+ - Clone/Download the repository. Navigate to the repository.  
+ - Run the following:
+    > pip install -r requirements.txt 
+    > python autoFiller.py <google_form_link> <path_to_the_xlsx_file>
---
+++ autoFiller.py
@@ -0,0 +1,47 @@
+import os, re, sys, time
+
+try:
+    import xlrd
+except:
+    print("No module name 'xlrd' found. \nInstall the module using 'pip install xlrd'")
+
+try:
+    import requests
+except:
+    print("No module name 'requests' found. \nInstall the module using 'pip install requests'")
+
+url = str(sys.argv[1])
+form = requests.get(url)
+soureCode = form.text
+
+formKeys = re.findall(r'entry\.[0-9_a-zA-Z]+', soureCode)
+
+spreadSheet = xlrd.open_workbook(str(sys.argv[2]))
+sheet = spreadSheet.sheet_by_index(0)
+
+start_idx = 0
+choice = int(input("Do your sheet have columns mentioned? \nIf yes, press 1. Else press 0. And enter.\n"))
+if(choice == 1):
+    start_idx = 1
+
+nums = [1, 2]
+
+for r in range(start_idx, sheet.nrows):
+    
+    formData = {}
+    data = sheet.row_values(r)
+    
+    for i in range(len(formKeys)):
+        if(i in nums):
+            data[i] = int(data[i])        
+        formData[formKeys[i]] = data[i]
+
+    print(formData)
+
+    try:
+        response = requests.post(url, formData)
+        print(response)
+        print('Form Submitted!')
+        time.sleep(2)
+    except:
+        print("Error Occured!")
